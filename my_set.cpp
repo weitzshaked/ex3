996ba4778b8b583d3f1a652398ca6992fc80e708
weitzshaked <weitzshaked@gmail.com>
2018-09-16 22:15:13 +0300

    balagan

--- my_set.cpp
+++ my_set.cpp
@@ -1,1 +1,53 @@
 #include "my_set.h"
+
+template<class T>
+my_set<T>::my_set(my_set &&other) noexcept
+{
+//	_string = other._string;
+//	other._string = nullptr;
+//	_length = other._length;
+
+}
+
+template<class T>
+my_set<T> &my_set<T>::operator=(my_set other)
+{
+	swap(*this, other);
+	return *this;
+}
+
+template<class T>
+my_set<T>::my_set(const my_set &other)
+{
+	_size = 0;
+	_head = nullptr;
+	for(iterator it = other.begin(); it != other.end(); it++)
+	{
+		insert(*it);
+	}
+}
+
+template<class T>
+const_iterator my_set<T>::find(const value_type &val) const
+{
+	for(iterator it = this->cbegin(); it != this->cend(); it++)
+	{
+		if(*it._head.getData() == val)
+		{
+			return it;
+		}
+	}
+}
+
+template<class T>
+template<class inputIt>
+my_set<T>::my_set(inputIt begin, inputIt end)
+{
+	_size =0;
+	for(iterator it = begin(); it != end(); it++)
+	{
+		insert(*it);
+		_size++;
+	}
+}
+
--- my_set.h
+++ my_set.h
@@ -2,6 +2,8 @@
 #define EX3_MY_SET_H
 
 #include <iostream>
+#include <bits/shared_ptr.h>
+#include <memory>
 
 
 template<class T>
@@ -9,42 +11,168 @@ class my_set
 {
 public:
 	typedef T value_type;
-	typedef T *iterator;
-	typedef const T *const_iterator;
-	typedef const T &const_reference;
+	typedef value_type &reference;
+	typedef const value_type &const_reference;
 
-	my_set();
+	class Node
+	{
+		std::shared_ptr<my_set> _parent;
+		std::shared_ptr<my_set> _right;
+		std::shared_ptr<my_set> _left;
+		value_type _data;
 
+		Node() = default;
+
+		value_type getData()
+		{
+			return _data;
+		}
+		Node next(std::shared_ptr<Node> other)
+		{
+			if(other.get().)
+		}
+
+		Node prev(std::shared_ptr<Node> other)
+		{
+
+		}
+	};
+
+//	typedef const std::bidirectional_iterator_tag iterator;
+//	typedef const std::bidirectional_iterator_tag const_iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+	class treeIterator
+	{
+		std::shared_ptr<Node> _pointer;
+
+	public:
+		explicit treeIterator(std::shared_ptr<Node> current):_pointer(current){};
+
+		treeIterator() = default;
+
+		~treeIterator() = default;
+
+		const std::shared_ptr<Node> &get_pointer() const
+		{
+			return _pointer;
+		}
+
+		treeIterator<T> &operator=(const treeIterator<T> &);
+
+		treeIterator<T> &operator=(treeIterator<T> &&);
+
+		virtual treeIterator operator++()
+		{
+			_pointer = _pointer.get()->next(_pointer);
+			return *this;
+		}
+
+		virtual treeIterator operator++(int)
+		{
+			treeIterator temp = *this;
+			_pointer = _pointer.get()->next(_pointer);
+			return temp;
+		}
+
+		virtual treeIterator operator--()
+		{
+			_pointer = _pointer.get()->prev(_pointer);
+			return *this;
+		}
+
+		virtual treeIterator operator--(int)
+		{
+			treeIterator temp = *this;
+			_pointer = _pointer.get()->prev(_pointer);
+			return temp;
+		}
+
+		reference operator*();
+
+//		T *operator->();
+//
+//		const T *operator->() const;
+//
+//		bool operator==(const treeIterator<T> &) const;
+//
+//		bool operator!=(const treeIterator<T> &) const;
+
+	};
+
+	/**
+	 * destructor
+	 */
+	~my_set()
+	{
+		clear();
+	};
+
+	/**
+	 * default constructor
+	 */
+	my_set() : _head(nullptr), _size(0) {};
+
+	/**
+	 * copy constructor
+	 * @param other
+	 */
 	my_set(const my_set &other);
 
-	my_set(const my_set &&other);
+	/**
+	 * move constructor
+	 * @param other
+	 */
+	my_set(my_set &&other) noexcept;
 
-	my_set(iterator begin, iterator end);
+	template<class inputIt>
+	my_set(inputIt begin, inputIt end);
 
-	const my_set &operator=(const my_set &other);
+	my_set &operator=(my_set other);
 
 	bool empty()
 	{
-		return size() != 0;
+		return size() == 0;
 	}
 
-	size_t size();
+	size_t size()
+	{
+		return _size;
+	}
 
 	constexpr void swap(T &a, T &b) noexcept;
 
+
+	template<class InputIt>
+	void insert(InputIt first, InputIt last);
+
 	std::pair<const_iterator, bool> insert(const value_type &val);
+
+	iterator insert(const_iterator hint, const value_type &value);
 
 	void clear() noexcept;
 
 	const_iterator find(const value_type &val) const;
 
+	const_iterator cbegin() const noexcept;
+
+	const_iterator cend() const noexcept;
+
+	const_reverse_iterator crbegin() const noexcept;
+
+	const_reverse_iterator crend() const noexcept;
+
+	friend void swap(my_set &first, my_set &second) noexcept;
+
+	iterator erase(const_iterator pos);
+
+	void erase(iterator first, iterator last);
+
 private:
-	class Node
-	{
-		my_set *right;
-		my_set *left;
-		T data;
-	};
+	std::weak_ptr<Node> _head;
+	size_t _size;
 };
 
 
